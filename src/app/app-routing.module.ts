5890df50addf2cd5a034b26b9662ac2337b8269c
Fek <nicolas.ramirezr@uqvirtual.edu.co>
2023-11-02 09:20:57 -0500

    commit 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './pagina/inicio/inicio.component';
+import { LoginMedicoComponent } from './pagina/login-medico/login-medico.component';
+import { LoginPacienteComponent } from './pagina/login-paciente/login-paciente.component';
+import { RegistroComponent } from './pagina/registro/registro.component';
+const routes: Routes = [
+{ path: "", component: InicioComponent },
+{ path: "loginMedico", component: LoginMedicoComponent },
+{ path: "loginPaciente", component: LoginPacienteComponent },
 
-const routes: Routes = [];
-
+{ path: "registro", component: RegistroComponent },
+{ path: "**", pathMatch: "full", redirectTo: "" }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
-})
-export class AppRoutingModule { }
+  })
+  export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pagina/inicio/inicio.component';
+import { LoginMedicoComponent } from './pagina/login-medico/login-medico.component';
+import { RegistroComponent } from './pagina/registro/registro.component';
+import { LoginPacienteComponent } from './pagina/login-paciente/login-paciente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    RegistroComponent,
+    LoginMedicoComponent,
+    LoginPacienteComponent
   ],
   imports: [
     BrowserModule,
