5deb6669471e178998ac41c5d6928033884a47d1
Fek <nicolas.ramirezr@uqvirtual.edu.co>
2023-11-02 09:13:31 -0500

    commit inicial

---
+++ src/app/pagina/login-medico/login-medico.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-medico',
+  templateUrl: './login-medico.component.html',
+  styleUrls: ['../../app.component.css']
+})
+export class LoginMedicoComponent {
+
+}
